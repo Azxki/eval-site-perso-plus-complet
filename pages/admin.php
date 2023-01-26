545037a2822541b54950a9d8ac9b08389a9736c2
azxki <cococoursan@gmail.com>
2023-01-26 14:36:24 +0100

    exo

--- pages/admin.php
+++ pages/admin.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Page de connexion</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../public/style.css">
 </head>
 <body>
-<form action="index.php" method="post" id="form">
+<form action="../public/index.php" method="post" id="form">
     <label for="user">Username</label>
-    <input type="text" name="user" id="user" required>
+    <input type="text" name="user" id="user">
 
     <label for="password">Password</label>
     <input type="password" name="password" id="password" required>
 
     <input type="submit" name="submit" id="connexion" value="Connexion">
-    <input type="submit" name="submit" id="create" value="Créer un compte">
 </form>
 </body>
 </html>
+
+<?php
 
